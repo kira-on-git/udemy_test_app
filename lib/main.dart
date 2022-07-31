c2d5161f5ec80816d2741bdc73df28058d99da16
kira-on-git <kira.kaunert@gmail.com>
2022-07-31 18:50:04 +0300

    3 functions for onPress added

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,41 @@
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  //const MyApp({Key? key}) : super(key: key);
+  void answerQuestion() {
+    print('Answer choosen!');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(title: const Text('udemy_test_app')),
-          body: Column(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('udemy_test_app'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               const Text('The question!'),
-              const RaisedButton(child: Text('Answer 1'), onPressed: null),
-              const RaisedButton(child: Text('Answer 2'), onPressed: null),
-              const RaisedButton(child: Text('Answer 3'), onPressed: null),
+              RaisedButton(onPressed: answerQuestion, child: Text('Answer 1')),
+              RaisedButton(
+                  onPressed: () => print('Answer 2 choosen!'),
+                  child: Text('Answer 2')),
+              RaisedButton(
+                  onPressed: () {
+                    //...
+                    print('Answer 3 choosen!');
+                  },
+                  child: Text('Answer 3')),
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
