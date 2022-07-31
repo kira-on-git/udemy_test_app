8851a3411da04ea208b397e0d3cf66893d2d40b9
kira-on-git <kira.kaunert@gmail.com>
2022-07-31 18:28:24 +0300

    2 answer-buttons added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      appBar: AppBar(title: const Text('udemy_test_app')),
-      body: const Text('Hello World!'),
-    ));
+          appBar: AppBar(title: const Text('udemy_test_app')),
+          body: Column(
+            children: [
+              const Text('The question!'),
+              const RaisedButton(child: Text('Answer 1'), onPressed: null),
+              const RaisedButton(child: Text('Answer 2'), onPressed: null),
+              const RaisedButton(child: Text('Answer 3'), onPressed: null),
+            ],
+          ),
+        ));
   }
 }
