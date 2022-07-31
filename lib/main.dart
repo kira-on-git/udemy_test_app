565001db0489f11aaf2824a602e4c93ea8ae5da8
kira-on-git <kira.kaunert@gmail.com>
2022-07-31 21:39:23 +0300

    6 stl refactored in stl + setState added

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   //const MyApp({Key? key}) : super(key: key);
   var questionIndex = 0;
+
   void answerQuestion() {
-    questionIndex = questionIndex + 1;
-    print(questionIndex);
+    setState(() {
+      questionIndex += 1;
+      print(questionIndex);
+    });
   }
 
   @override
